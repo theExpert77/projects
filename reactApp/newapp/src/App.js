f5d99ab123b6f1e3d99405a69b5a44d7d0b288fe
theExpert77 <shubham.sagrawal7@gmail.com>
2025-03-05 00:46:54 +0530

    revert route for git pages

--- reactApp/newapp/src/App.js
+++ reactApp/newapp/src/App.js
@@ -1,23 +1,24 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import TextForm from "./Components/TextForm";
-import About from "./Components/About";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import About from "./Components/About";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Navbar myName="TextUtils" />;
-        <Routes>
-          <Route exact path="/about" element={<About />} />
+      {/* <BrowserRouter> */}
+      <Navbar myName="TextUtils" />;{/* <Routes> */}
+      {/* <Route exact path="/about" element={<About />} />
           <Route
             exact
-            path="/"
-            element={<TextForm title="Enter the text to analyze below" />}
-          />
+            path="/" */}
+      {/* element={ */}
+      <TextForm title="Enter the text to analyze below" />
+      {/* } */}
+      {/* />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
     </>
   );
 }
--- reactApp/newapp/src/Components/Navbar.js
+++ reactApp/newapp/src/Components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 export default function Navbar(myN) {
   return (
@@ -24,14 +24,14 @@ export default function Navbar(myN) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">
+                <a className="nav-link active" aria-current="page" href="/">
                   Home
-                </Link>
+                </a>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/about">
+                <a className="nav-link" href="/about">
                   About
-                </Link>
+                </a>
               </li>
             </ul>
             <form className="d-flex" role="search">
