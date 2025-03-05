6c701c269c5b96547260155db5ea6ff3b610d867
theExpert77 <shubham.sagrawal7@gmail.com>
2025-03-05 15:31:11 +0530

    fix

--- reactApp/newapp/src/Components/Navbar.js
+++ reactApp/newapp/src/Components/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar(myN) {
     <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">
+          <a className="navbar-brand" href="#">
             {myN.myName}
           </a>
           <button
@@ -24,12 +24,12 @@ export default function Navbar(myN) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
+                <a className="nav-link active" aria-current="page" href="#">
                   Home
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/about">
+                <a className="nav-link" href="#">
                   About
                 </a>
               </li>
